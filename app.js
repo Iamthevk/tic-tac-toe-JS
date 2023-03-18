c29aee0ec166950851aceb52769ba198ffec9587
Vijay Kumar <vijay.vuex@gmail.com>
2023-03-18 18:47:48 +0530

    circle & cross renders one after other onClick

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
  const startCells = [
     "","","","","","","","",""
  ]
+ infoDisplay.textContent = "Circle go first";
+
+ let activeIcon = "circle"
 
  //here with this function we are creating board and gave index to each 9 cell in board
  function createBoard() {
@@ -20,8 +23,12 @@
 
  // when we click on cell the below function will call & append Icon to the cell in Board
  function addMark(e){
-    console.log(e.target)
+    // console.log(e.target)
     const displayIcon = document.createElement("div")
-    displayIcon.classList.add("circle")
+    displayIcon.classList.add(activeIcon)
     e.target.append(displayIcon)
+    activeIcon = activeIcon === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = "its now " + activeIcon + "'s turn"
+    // we have to add remove event so user can't click again on selected cell
+    e.target.removeEventListener("click", addMark)
  }
